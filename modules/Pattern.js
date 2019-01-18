366a27bb28bd3a3bb0338d1ad3a526eb042c7122
t638403 <e.j.vd.hooven@gmail.com>
2019-01-18 11:24:43 +0100

    More readable

--- modules/Pattern.js
+++ modules/Pattern.js
@@ -18,24 +18,29 @@ class Pattern extends Transform {
 
 	_transform(partialMidiMsg, encoding, done) {
 		const tick = this.pattern.next().value;
-		const noteOff = {msg:'noteOff'};
-		const noteOn = {msg:'noteOn'};
+
+		const noteOn = Object.assign({}, partialMidiMsg, {msg:'noteOn'});
+
+		// Note off basically means duplicating the previous msg while maintaining the current time, and setting the
+		// message to noteOff.
+		const noteOff = Object.assign({}, this.prevPartialMidiMsg, {msg:'noteOff', t: partialMidiMsg.t});
+
 		switch(tick) {
 			case 'x':
 				if(this.prevTick === 'x' || this.prevTick === '=') {
 					// There must be a tiny delay between noteOff and noteOn or machines start to expose undesirable
-					// behaviour. 3ms seems to be long enough for the machines to react, and it is short enough
-					// for us humans to notice.
-					this.push(Object.assign({}, this.prevPartialMidiMsg, noteOff, {t:partialMidiMsg.t - BigInt(3)}));
-
-					this.push(Object.assign({}, partialMidiMsg, noteOn));
+					// behaviour, like hanging notes. 3ms seems to be long enough for our mighty machines to react, and
+					// it is short enough for us mighty humans to not notice.
+					noteOff.t = noteOff.t - BigInt(3);
+					this.push(noteOff);
+					this.push(noteOn);
 				} else {
-					this.push(Object.assign({}, partialMidiMsg, noteOn));
+					this.push(noteOn);
 				}
 				break;
 			case '.':
 				if(this.prevTick === '=' || this.prevTick === 'x') {
-					this.push(Object.assign({}, this.prevPartialMidiMsg, noteOff));
+					this.push(noteOff);
 				}
 				break;
 		}
