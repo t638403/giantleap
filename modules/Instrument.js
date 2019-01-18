47ef15bf6fa12a35d12ce4448a123373351a1da0
t638403 <e.j.vd.hooven@gmail.com>
2019-01-18 11:21:08 +0100

    Adding Instrument

---
+++ modules/Instrument.js
@@ -0,0 +1,19 @@
+const { Transform } = require('stream');
+
+class Instrument extends Transform {
+
+	constructor(deviceNo, midiChannel) {
+		super({objectMode:true});
+
+		this.deviceNo = deviceNo;
+		this.midiChannel = midiChannel;
+	}
+
+	_transform(partialMidiMsg, _enc, next) {
+		this.push(Object.assign({}, partialMidiMsg, {device:this.deviceNo, channel:this.midiChannel}));
+		next();
+	}
+
+}
+
+module.exports = Instrument;
