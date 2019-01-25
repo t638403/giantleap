d5c6bbfab0ac9b830b6521bcef8e000d139721f8
t638403 <e.j.vd.hooven@gmail.com>
2019-01-25 18:09:38 +0100

    First working version!!

--- run.js
+++ run.js
@@ -3,64 +3,97 @@ const Metronome = require('@giantleap/Metronome'),
 	Velocity    = require('@giantleap/Velocity'),
 	Note        = require('@giantleap/Note'),
 	Instrument  = require('@giantleap/Instrument'),
+	Stitch      = require('@giantleap/Stitch'),
 	MidiMsgr    = require('@giantleap/MidiMsgr'),
 	MidiOut     = require('@giantleap/MidiOut'),
 	TimeSort    = require('@giantleap/TimeSort'),
+	Tap         = require('@giantleap/Tap'),
 	Json        = require('@giantleap/JSON')
 ;
 
-const out = new MidiOut();
-const ts  = new TimeSort();
+const out = new Stitch();
+out
+	// .pipe(new Tap())
+	.pipe(new TimeSort())
+	.pipe(new MidiOut())
+;
 
 // electribe notes
 const en = require('./modules/instruments/electribe').notes;
-const electribe = new Instrument(2, 2);
-const m = new Metronome(120, 4);
+const electribe = () => new Instrument(3, 2);
+const arp = () => new Instrument(1, 1);
+const m120 = () => new Metronome(120, 4);
 
-m
+
+m120()
 	.pipe(new Pattern([
-		'x.xx'
+		'x...'
+	]))
+	.pipe(new Note('C2'))
+	.pipe(arp())
+	.pipe(new MidiMsgr())
+	.pipe(out)
+;
+
+m120()
+	.pipe(new Pattern([
+		'x...'
+	]))
+	.pipe(new Note(en.S1))
+	.pipe(electribe())
+	.pipe(new MidiMsgr())
+	.pipe(out)
+;
+
+m120()
+	.pipe(new Pattern([
+		'x..x'
 	]))
 	.pipe(new Velocity('036'))
 	.pipe(new Note(en.HH_CLOSE))
-	.pipe(electribe)
+	.pipe(electribe())
 	.pipe(new MidiMsgr())
-	.pipe(ts)
 	.pipe(out)
 ;
 
-m
+m120()
+	.pipe(new Pattern([
+		'..x.'
+	]))
+	.pipe(new Note(en.HH_OPEN))
+	.pipe(electribe())
+	.pipe(new MidiMsgr())
+	.pipe(out)
+;
+
+m120()
 	.pipe(new Pattern([
 		'....x...'
 	]))
-	// .pipe(new Velocity('123454321'))
 	.pipe(new Note(en.CLAP))
-	.pipe(electribe)
+	.pipe(electribe())
 	.pipe(new MidiMsgr())
-	.pipe(ts)
 	.pipe(out)
 ;
 
-m
+m120()
 	.pipe(new Pattern([
 		'x'
 	]))
 	.pipe(new Velocity('123454321'))
 	.pipe(new Note(en.S2))
-	.pipe(electribe)
+	.pipe(electribe())
 	.pipe(new MidiMsgr())
-	.pipe(ts)
 	.pipe(out)
 ;
 
-m
+m120()
 	.pipe(new Pattern([
 		'x..x..x...x.xx..x..x..x...x.x...'
 	]))
 	.pipe(new Velocity('787987968697968'))
 	.pipe(new Note(en.S3))
-	.pipe(electribe)
+	.pipe(electribe())
 	.pipe(new MidiMsgr())
-	.pipe(ts)
 	.pipe(out)
 ;
