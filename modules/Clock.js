468e87618fbf60a8ad8ad476469dcd90d4abc26c
t638403 <e.j.vd.hooven@gmail.com>
2019-01-26 10:38:08 +0100

    Midi clock

---
+++ modules/Clock.js
@@ -0,0 +1,17 @@
+const { Transform } = require('stream');
+const RingBuffer    = require('@giantleap/utils/RingBuffer');
+const isArray       = require('lodash/isArray');
+
+
+class Clock extends Transform {
+	constructor() {
+		super({objectMode:true, highWaterMark:5000});
+	}
+
+	_transform(partialMidiMsg, _enc, next) {
+		this.push(Object.assign({}, partialMidiMsg, {msg:'clock'}));
+		next();
+	}
+}
+
+module.exports = Clock;
