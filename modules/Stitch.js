4d242cd08869c94348187c5a59dea3aedb0d726f
erik <e.j.vd.hooven@gmail.com>
2019-02-03 23:17:02 +0100

    Stitching process now pushes t_smallest

--- modules/Stitch.js
+++ modules/Stitch.js
@@ -1,13 +1,26 @@
 const { Transform } = require('stream');
 
 const max = (a, b) => a > b ? a : b;
+const min = (a, b) => a < b ? a : b;
+
+const nextActiveStreamIndex = (msgs) => {
+	let leastIndex = 0;
+	let least = Infinity;
+	for (let i = 0; i < msgs.length; i++) {
+		least = min(least, msgs[i].t);
+		if(least === msgs[i].t) {
+			leastIndex = i;
+		}
+	}
+	return leastIndex;
+};
 
 class Stitch extends Transform {
 	constructor() {
 		super({objectMode:true});
 		this.activeStreamIndex = 0;
 		this.streams = [];
-		this.maxT = 0;
+		this.msgs = [];
 
 		this.on('pipe', src => {
 			if(this.streams.length > 0) {
@@ -20,22 +33,36 @@ class Stitch extends Transform {
 
 	_transform(msg, _enc, next) {
 
-		this.maxT = max(this.maxT, msg.t);
+		if(this.msgs.length < this.streams.length) {
 
-		// console.log(`d:${msg.device}, ${msg.msg} - ${this.maxT}`);
-
-		if(msg.t >= this.maxT) {
+			this.msgs.push(msg);
 			this.streams[this.activeStreamIndex].pause();
-			this.activeStreamIndex++;
-			if(this.activeStreamIndex === this.streams.length) {
-				this.activeStreamIndex = 0;
-			}
-			this.streams[this.activeStreamIndex].resume();
-		}
 
-		this.push(msg);
+			this.activeStreamIndex++;
+			if (this.streams[this.activeStreamIndex]) {
+				this.streams[this.activeStreamIndex].resume();
+			} else {
+				this.activeStreamIndex = nextActiveStreamIndex(this.msgs);
+				this.streams[this.activeStreamIndex].resume();
+				this.push(this.msgs[this.activeStreamIndex]);
+			}
+
+		} else {
+
+			this.msgs[this.activeStreamIndex] = msg;
+			this.streams[this.activeStreamIndex].pause();
+			this.activeStreamIndex = nextActiveStreamIndex(this.msgs);
+			this.push(this.msgs[this.activeStreamIndex]);
+			this.streams[this.activeStreamIndex].resume();
+
+		}
 		next();
 	}
+
+	// _final(done) {
+	//
+	// 	done();
+	// }
 }
 
 module.exports = Stitch;
