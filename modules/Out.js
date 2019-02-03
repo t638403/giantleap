4a21eaf76ab9d050cde89eee33274024f2e6e912
erik <e.j.vd.hooven@gmail.com>
2019-02-03 23:19:38 +0100

    Added some debugging stuff

--- modules/Out.js
+++ modules/Out.js
@@ -4,8 +4,20 @@ const Stitch      = require('@giantleap/Stitch'),
 	MidiMsgr    = require('@giantleap/MidiMsgr'),
 	MidiOut     = require('@giantleap/MidiOut'),
 	TimeSort    = require('@giantleap/TimeSort'),
-	Tap         = require('@giantleap/Tap')
+	Tap         = require('@giantleap/Tap'),
+	Pick         = require('@giantleap/Pick'),
+	Json        = require('@giantleap/JSON')
 ;
+
+const sto = (ms) => new Transform({
+	objectMode:true,
+	transform(data, _enc, next) {
+		setTimeout(() => {
+			this.push(data);
+			next();
+		}, ms);
+	}
+});
 
 class Out extends Stitch {
 
@@ -18,8 +30,13 @@ class Out extends Stitch {
 		Out.instance = this;
 		this
 			.pipe(new MidiMsgr())
-			.pipe(new TimeSort())
+			// .pipe(new TimeSort())
+			// .pipe(sto(100))
+			// .pipe(new Pick(['t', 'msg']))
+			// .pipe(Json.stringify)
+			// .pipe(process.stdout)
 			.pipe(new MidiOut())
+		;
 	}
 
 }
