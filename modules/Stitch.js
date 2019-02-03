369c9b0f03847ab39712ba66d076f8774b5241b9
t638403 <e.j.vd.hooven@gmail.com>
2019-02-03 14:24:41 +0100

    high speed lanes (like the clock msgs) need more throughput

--- modules/Stitch.js
+++ modules/Stitch.js
@@ -1,10 +1,13 @@
 const { Transform } = require('stream');
 
+const max = (a, b) => a > b ? a : b;
+
 class Stitch extends Transform {
 	constructor() {
 		super({objectMode:true});
 		this.activeStreamIndex = 0;
 		this.streams = [];
+		this.maxT = 0;
 
 		this.on('pipe', src => {
 			if(this.streams.length > 0) {
@@ -15,16 +18,22 @@ class Stitch extends Transform {
 
 	}
 
-	_transform(data, _enc, next) {
+	_transform(msg, _enc, next) {
 
-		this.streams[this.activeStreamIndex].pause();
-		this.activeStreamIndex++;
-		if(this.activeStreamIndex === this.streams.length) {
-			this.activeStreamIndex = 0;
+		this.maxT = max(this.maxT, msg.t);
+
+		// console.log(`d:${msg.device}, ${msg.msg} - ${this.maxT}`);
+
+		if(msg.t >= this.maxT) {
+			this.streams[this.activeStreamIndex].pause();
+			this.activeStreamIndex++;
+			if(this.activeStreamIndex === this.streams.length) {
+				this.activeStreamIndex = 0;
+			}
+			this.streams[this.activeStreamIndex].resume();
 		}
-		this.streams[this.activeStreamIndex].resume();
 
-		this.push(data);
+		this.push(msg);
 		next();
 	}
 }
