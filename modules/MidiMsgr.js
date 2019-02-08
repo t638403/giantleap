f3f311ff875e4982a97f920bd69099233fa2fbd3
t638403 <e.j.vd.hooven@gmail.com>
2019-02-08 14:27:06 +0100

    Buffer note off and push when its time has come

--- modules/MidiMsgr.js
+++ modules/MidiMsgr.js
@@ -1,4 +1,6 @@
 const { Transform } = require('stream');
+const FastPriorityQueue = require("fastpriorityqueue");
+
 const {
 	noteOn,
 	noteOff,
@@ -11,9 +13,14 @@ class MidiMsgr extends Transform {
 
 	constructor() {
 		super({objectMode:true});
+		this.pq = new FastPriorityQueue( (a, b) => a.t < b.t );
 	}
 
 	_transform(msg, _enc, next) {
+
+		while(!this.pq.isEmpty() && this.pq.peek().t < msg.t) {
+			this.push(this.pq.poll());
+		}
 
 		switch(msg.msg) {
 			case 'note':
@@ -22,7 +29,7 @@ class MidiMsgr extends Transform {
 					t:msg.t,
 					msg:noteOn(msg.channel, msg.key || 'C2', msg.velocity || 100)
 				});
-				this.push({
+				this.pq.add({
 					device: msg.device,
 					t:msg.tEnd,
 					msg:noteOff(msg.channel, msg.key || 'C2', msg.velocity || 100)
