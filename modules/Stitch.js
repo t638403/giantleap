fcfbbc754b04e25aa8860c97d3f60df7b3161394
t638403 <e.j.vd.hooven@gmail.com>
2019-02-08 14:46:22 +0100

    Stitch 2.0

--- modules/Stitch.js
+++ modules/Stitch.js
@@ -1,4 +1,4 @@
-const { Transform } = require('stream');
+const { Readable, Transform } = require('stream');
 
 const max = (a, b) => a > b ? a : b;
 const min = (a, b) => a < b ? a : b;
@@ -16,53 +16,42 @@ const nextActiveStreamIndex = (msgs) => {
 };
 
 class Stitch extends Transform {
-	constructor() {
+	constructor(streams) {
 		super({objectMode:true});
-		this.activeStreamIndex = 0;
-		this.streams = [];
+		this.streams = streams;
 		this.msgs = [];
-
-		this.on('pipe', src => {
-			if(this.streams.length > 0) {
-				src.pause();
-			}
-			this.streams.push(src);
-		});
-
-	}
-
-	_transform(msg, _enc, next) {
-
-		if(this.msgs.length < this.streams.length) {
-
-			this.msgs.push(msg);
-			this.streams[this.activeStreamIndex].pause();
-
-			this.activeStreamIndex++;
-			if (this.streams[this.activeStreamIndex]) {
-				this.streams[this.activeStreamIndex].resume();
-			} else {
-				this.activeStreamIndex = nextActiveStreamIndex(this.msgs);
-				this.streams[this.activeStreamIndex].resume();
-				this.push(this.msgs[this.activeStreamIndex]);
-			}
-
-		} else {
-
-			this.msgs[this.activeStreamIndex] = msg;
-			this.streams[this.activeStreamIndex].pause();
-			this.activeStreamIndex = nextActiveStreamIndex(this.msgs);
-			this.push(this.msgs[this.activeStreamIndex]);
-			this.streams[this.activeStreamIndex].resume();
-
+		for(const i in this.streams) {
+			this.streams[i].pipe(this);
+			this.streams[i].pause();
+			this.streams[i].isReady = () => new Promise(resolve => this.streams[i].on('readable', resolve));
 		}
-		next();
+		this.activeStreamIndex = null;
+		this._allStreamsReady = false;
+		this.allStreamsReady().then(() => {
+			for (const stream of this.streams) {
+				this.msgs.push(stream.read());
+			}
+			this.activeStreamIndex = nextActiveStreamIndex(this.msgs);
+		})
 	}
 
-	// _final(done) {
-	//
-	// 	done();
-	// }
+	allStreamsReady() {
+		return this._allStreamsReady || Promise
+			.all(this.streams.map(s => s.isReady()))
+			.then(() => this._allStreamsReady = true)
+			.then(() => true)
+		;
+	}
+
+	_transform(_, _enc, next) {
+		Promise.resolve(this.allStreamsReady())
+			.then(() => {
+				this.push(this.msgs[this.activeStreamIndex]);
+				this.msgs[this.activeStreamIndex] = this.streams[this.activeStreamIndex].read();
+				this.activeStreamIndex = nextActiveStreamIndex(this.msgs);
+				next();
+			});
+	}
 }
 
 module.exports = Stitch;
--- run.js
+++ run.js
@@ -1,3 +1,5 @@
+const { Transform } = require('stream');
+
 const Metronome = require('@giantleap/Metronome'),
 	Pattern     = require('@giantleap/Pattern'),
 	Velocity    = require('@giantleap/Velocity'),
@@ -10,9 +12,11 @@ const Metronome = require('@giantleap/Metronome'),
 	Device      = require('@giantleap/Device'),
 	Instrument  = require('@giantleap/Instrument'),
 	Electribe   = require('@giantleap/Electribe'),
-	Out         = require('@giantleap/Out'),
+	Pick        = require('@giantleap/Pick'),
 	MidiOut     = require('@giantleap/MidiOut'),
-	JSON        = require('@giantleap/JSON'),
+	Stitch      = require('@giantleap/Stitch'),
+	MidiMsgr    = require('@giantleap/MidiMsgr'),
+	Json        = require('@giantleap/JSON'),
 	RingBuffer  = require('@giantleap/utils/RingBuffer')
 ;
 
@@ -29,95 +33,97 @@ const doepfer   = () => new Instrument(/USB Device 0x7cd:0xfe06 \d\d:0/, 1);
 
 const sendMidiClock = (bpm) => Object
 	.values(MidiOut.availablePorts())
-	.forEach(port => (new Metronome(bpm, 24))
+	.map(port => (new Metronome(bpm, 24))
 		.pipe(new Clock())
 		.pipe(new Device(port))
-		.pipe(new Out())
 	)
 ;
 
 // CLOCK ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const bpm = 8;
-sendMidiClock(bpm);
+const bpm = 120;
+const clocks = sendMidiClock(bpm);
 const m120 = () => new Metronome(bpm, 4);
 const m120_n = (n) => new Metronome(bpm, n);
 
+
 // SONG ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const streams = [
 
-m120()
-	.pipe(new Pattern('xxxxxxxx'))
-	.pipe(new Note([
-		Electribe.S1,
-		Electribe.HH_CLOSE,
-		Electribe.HH_OPEN,
-		Electribe.HH_CLOSE,
-		Electribe.S1,
-		Electribe.HH_CLOSE,
-		Electribe.HH_OPEN,
-		Electribe.HH_CLOSE,
-	]))
-	.pipe(electribe())
-	.pipe(new Out())
-;
+	...clocks,
 
-m120()
-	.pipe(new Pattern('..x..x.x..x..x.x..x..x.x..x..x..'))
-	.pipe(new Note(Electribe.CLAP))
-	.pipe(electribe())
-	.pipe(new Out())
-;
+	m120()
+		.pipe(new Pattern('x..x..x...x..x......x..x..x.x...'))
+		.pipe(new Note([
+			Electribe.S1
+		]))
+		.pipe(electribe()),
 
-m120()
-	.pipe(new Pattern([
-		'..x..x.x..x..x.x..x..x.x..x.x.x...x.'
-	]))
-	.pipe(new Note('C3'))
-	.pipe(arp())
-	.pipe(new Out())
-;
+	m120()
+		.pipe(new Pattern('xxxxxxxx'))
+		.pipe(new Note([
+			Electribe.HH_OPEN,
+			Electribe.HH_CLOSE,
+			Electribe.HH_OPEN,
+			Electribe.HH_CLOSE,
+			Electribe.HH_OPEN,
+			Electribe.HH_CLOSE,
+			Electribe.HH_OPEN,
+			Electribe.HH_CLOSE,
+		]))
+		.pipe(electribe()),
 
-m120()
-	.pipe(new Pattern([
-		'....x.......x....x.......x..x.......x....x..x....x..x..x.x..x...'
-	]))
-	.pipe(new Velocity('1'))
-	.pipe(new Note('C7'))
-	.pipe(ensoniq1())
-	.pipe(new Out())
-;
+	m120()
+		.pipe(new Pattern('..x..x.x..x..x.x..x..x.x..x..x..'))
+		.pipe(new Note(Electribe.CLAP))
+		.pipe(electribe()),
 
-m120()
-	.pipe(new Pattern([
-		'..x.'
-	]))
-	.pipe(new Velocity('1'))
-	.pipe(new Note('A#3'))
-	.pipe(ensoniq1())
-	.pipe(new Out())
-;
+	m120()
+		.pipe(new Pattern([
+			'..x..x.x..x..x.x..x..x.x..x.x.x...x.'
+		]))
+		.pipe(new Note('C3'))
+		.pipe(arp()),
 
-m120()
-	.pipe(new Pattern([
-		'x...'
-	]))
-	.pipe(new Velocity('9'))
-	.pipe(new Note('C2'))
-	.pipe(ensoniq2())
-	.pipe(new Out())
-;
+	m120()
+		.pipe(new Pattern([
+			'....x.......x....x.......x..x.......x....x..x....x..x..x.x..x...'
+		]))
+		.pipe(new Velocity('1'))
+		.pipe(new Note('C7'))
+		.pipe(ensoniq1()),
 
-m120()
-	.pipe(new Pattern([
-		'........x..x..x=',
-		'============....'
-	]))
-	.pipe(new Chord([
-		// ['F2', 'G#2', 'A#2', 'C#3', 'E3']
-		['E4', 'F#4', 'G#4', 'A4'],
-		['E4', 'F#4', 'G#4', 'A4'],
-		['G3', 'A#3', 'C4', 'D4'],
-	], 1))
-	.pipe(yamaha())
-	.pipe(new Out())
+	m120()
+		.pipe(new Pattern([
+			'..x.'
+		]))
+		.pipe(new Velocity('1'))
+		.pipe(new Note('A#3'))
+		.pipe(ensoniq1()),
+
+	m120()
+		.pipe(new Pattern([
+			'x...'
+		]))
+		.pipe(new Velocity('9'))
+		.pipe(new Note('C2'))
+		.pipe(ensoniq2()),
+
+	m120()
+		.pipe(new Pattern([
+			'........x..x..x=',
+			'============....'
+		]))
+		.pipe(new Chord([
+			// ['F2', 'G#2', 'A#2', 'C#3', 'E3']
+			['E4', 'F#4', 'G#4', 'A4'],
+			['E4', 'F#4', 'G#4', 'A4'],
+			['G3', 'A#3', 'C4', 'D4'],
+		], 1))
+		.pipe(yamaha())
+];
+
+(new Stitch(streams))
+	.pipe(new MidiMsgr())
+	.pipe(new MidiOut())
 ;
