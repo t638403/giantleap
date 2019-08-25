88667d00e7b5066874f558869f7f1b34efa126bd
t638403 <e.j.vd.hooven@gmail.com>
2019-08-25 18:25:29 +0200

    Pattern variations

--- modules/Pattern.js
+++ modules/Pattern.js
@@ -6,15 +6,46 @@ const { Transform } = require('stream'),
 
 class Pattern extends Transform {
 
-	constructor(patStr) {
+	constructor(patStr, variations=false) {
 		super({objectMode:true});
-		if(isArray(patStr)) {
-			patStr = patStr.join('');
-		}
 		this.prevStepType = null;
+
+		// join the array of strings patterns.
+		patStr = patStr.join('');
 
 		// A hit is a key press with a duration (eventually)
 		this.hit = null;
+
+		// Variations are variations on the main pattern every x beats. They are key value pairs where the key represents
+		// the number of beats when the variation should play, the value is the variation pattern. At this point, the
+		// variation pattern must be exactly as long as the main pattern.
+		//
+		// Example:
+		// ...
+		// .pipe(new Pattern([
+		// 		'x..x....' // <- main pattern
+		// 	], {
+		// 		4:'x..x..x.',  // <- A variation played every 4 beats
+		// 		16:'xx.x..xx', // <- A variation played every 16 beats
+		// 		32:'x.x.x.x.'  // <- A variation played every 32 beats
+		// 	}))
+		// ...
+		//
+		if(variations) {
+			const enlargedPatStr = [];
+			const variationIndexes = Object.keys(variations);
+			const longestIndex = Math.max(...variationIndexes);
+			for(let i=1; i <= longestIndex; i++) {
+				enlargedPatStr[i-1] = patStr;
+				for(const variationIndex of variationIndexes) {
+					if(i % variationIndex === 0) {
+						enlargedPatStr[i-1] = variations[variationIndex]
+					}
+				}
+			}
+			patStr = [].concat(...enlargedPatStr).join('');
+		}
+
 		this.pattern = RingBuffer(patStr.split(''));
 	}
 
