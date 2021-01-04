954175737d851666ae3852231a9878e95056a676
t638403 <e.j.vd.hooven@gmail.com>
2021-01-04 11:32:15 +0100

    Use custom err class for better catch handlers

--- modules/utils/Msgr.js
+++ modules/utils/Msgr.js
@@ -1,5 +1,12 @@
 const isNumber = require('@giantleap/utils/underdash/isNumber');
 const isString = require('@giantleap/utils/underdash/isString');
+
+class MsgrError extends Error {
+  constructor(msg) {
+    super(`MsgrError: ${msg}`);
+  }
+}
+
 /**
  * Implementation of MIDI protocol
  * https://www.midi.org/specifications-old/item/table-1-summary-of-midi-message
@@ -50,8 +57,8 @@ const helperFns = {
 
     const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
-    if(typeof nr !== 'number') throw new Error('input must be a number');
-    if( !(nr > 20 && nr < 109) ) throw new Error('input should be 20 < nr < 109');
+    if(typeof nr !== 'number') throw new MsgrError('input must be a number');
+    if( !(nr > 20 && nr < 109) ) throw new MsgrError('input should be 20 < nr < 109');
 
     const octave = Math.floor(nr/12);
     const key = (nr - (octave * 12));
@@ -68,12 +75,12 @@ const helperFns = {
 
     const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
-    if(typeof str !== 'string') throw new Error('input must be a string');
-    if(!/^[ABCDEFG](#|)\d$/.test(str)) {throw new Error('Invalid note string ' + str);}
+    if(typeof str !== 'string') throw new MsgrError('input must be a string');
+    if(!/^[ABCDEFG](#|)\d$/.test(str)) {throw new MsgrError('Invalid note string ' + str);}
 
     const key = str.replace(/\d$/, '');
     const nr = keys.indexOf(key);
-    if(nr === -1) throw new Error('Invalid key ' + key);
+    if(nr === -1) throw new MsgrError('Invalid key ' + key);
 
     const octave = (parseInt(str.replace(/^.*?(\d)$/, '$1')) + 1);
 
@@ -90,10 +97,10 @@ const helperFns = {
     const names = {'C':12,'C#':13,'D':14,'D#':15,'E':16,'F':17,'F#':18,'G':19,'G#':20,'A':21,'A#':22,'B':23};
     const parts = nameAndOctave.toUpperCase().match(/^([CDEFGAB](|#))((|-)\d)$/);
     const name = parts[1];
-    if(!(name in names)) {throw new Error(`Invalid note name '${nameAndOctave}'`);}
+    if(!(name in names)) {throw new MsgrError(`Invalid note name '${nameAndOctave}'`);}
     const octave = parseInt(parts[3]);
     const noteNr = names[name] + (octave * 12);
-    if(noteNr<0 || noteNr > 127) {throw new Error('Note nr out of bounds, must be 0-127')}
+    if(noteNr<0 || noteNr > 127) {throw new MsgrError('Note nr out of bounds, must be 0-127')}
     return noteNr
   },
 
@@ -127,10 +134,10 @@ const channelVoiceMsgs = {
     return [Dn, note_number, velocity];
   },
   ctrl: (channelNr, ctrl_number, value) => {
-    if(!isNumber(ctrl_number) || ctrl_number < 0 || ctrl_number > 127) {throw new Error('Ctrl index out of bounds, must be 0-127')}
-    if(!isNumber(value) || value < 0 || value > 127) {throw new Error('Ctrl value must be 0-127')}
+    if(!isNumber(ctrl_number) || ctrl_number < 0 || ctrl_number > 127) {throw new MsgrError('Ctrl index out of bounds, must be 0-127')}
+    if(!isNumber(value) || value < 0 || value > 127) {throw new MsgrError('Ctrl value must be 0-127')}
     const Bn = parseInt('0xB0', 16) + (channelNr - 1);
-    if(!isNumber(Bn) || Bn < 176 || Bn > 191) {throw new Error('Ctr Bn index out of bounds, must be 176-191');}
+    if(!isNumber(Bn) || Bn < 176 || Bn > 191) {throw new MsgrError('Ctr Bn index out of bounds, must be 176-191');}
     return [Bn, ctrl_number, value];
   },
   programChange: (channelNr, program_number) => {
@@ -139,7 +146,7 @@ const channelVoiceMsgs = {
     return [Cn, program_number];
   },
   channelPressure: (channelNr, value) => {
-    if(!isNumber(value) || value < 0 || value > 127) {throw new Error('Ctrl value must be 0-127')}
+    if(!isNumber(value) || value < 0 || value > 127) {throw new MsgrError('Ctrl value must be 0-127')}
     const channel_pressure = parseInt('11010000', 2) + (channelNr - 1);
     return [channel_pressure, value]
   },
@@ -213,19 +220,19 @@ const channelModeMsgs = {
 const systemCommonMsgs = {
   systemExclusive: () => {
     const Bn = parseInt('11110000', 2);
-    throw new Error('Not implemented');
+    throw new MsgrError('Not implemented');
   },
   midiTimeCodeQuarterFrame: (msgType, values) => {
     const Bn = parseInt('11110001', 2);
-    throw new Error('Not implemented');
+    throw new MsgrError('Not implemented');
   },
   songPositionPointer: (song) => {
     const Bn = parseInt('11110010', 2);
-    throw new Error('Not implemented');
+    throw new MsgrError('Not implemented');
   },
   songSelect: (value) => {
     const Bn = parseInt('11110011', 2);
-    if(!isNumber(value) || value < 0 || value > 127) {throw new Error('Ctrl value must be 0-127')}
+    if(!isNumber(value) || value < 0 || value > 127) {throw new MsgrError('Ctrl value must be 0-127')}
     return [Bn, value];
   },
   tuneRequest: () => [parseInt('11110110', 2)],
@@ -247,4 +254,5 @@ module.exports = {
   ...systemCommonMsgs,
   ...systemRealTimeMsgs,
 	...helperFns,
+  MsgrError
 };
